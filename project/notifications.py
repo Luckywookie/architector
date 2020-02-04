578597f80a7f01c70e53e07faef1ccc7d63eb522
obelykh <olga.belykh@gmail.com>
2020-02-04 23:17:33 +0300

    Fix bug: process exceptions for notifications services errors

--- project/notifications.py
+++ project/notifications.py
@@ -1,16 +1,28 @@
 import aiohttp
+from aiohttp import ClientProxyConnectionError, ClientError
 
 
 async def send_email(config, recipient, message):
     url = config.EMAIL_SERVICE
     request = {"recipient": recipient, "message": message}
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, json=request, verify_ssl=False) as response:
-            return await response.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, json=request, verify_ssl=False) as response:
+                return await response.json()
+    except ClientError as ex:
+        print(ex)
+        return {'error': ex}
 
 
 async def send_telegram(config, message):
     url = config.PUSH_SERVICE
-    request = {"chat_id": config.chat_id, "message": message}
-    async with aiohttp.ClientSession().post(url, json=request, verify_ssl=False) as response:
-        return await response.json()
+    request = {"chat_id": config.CHAT_ID, "message": message}
+    try:
+        async with aiohttp.ClientSession().post(url, json=request, verify_ssl=False) as response:
+            return await response.json()
+    except ClientProxyConnectionError as ex:
+        print(ex)
+    except ClientError as ex:
+        print(ex)
+        return {'error': ex}
+
